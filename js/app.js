620ac29e9595ce23ffc23229c969e6af371de84a
Taneros <fricentaurus@GMAIL.COM>
2021-11-30 02:19:28 +0300

    refactor: move comments to TODO section

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-//Document is the DOM can be accessed in the console with document.window.
-// Tree is from the top, html, body, p etc.
-
-//Problem: User interaction does not provide the correct results.
-//Solution: Add interactivity so the user can manage daily tasks.
-//Break things down into smaller steps and take each step at a time.
+//TODO 
+/**
+  * Issues with usability don't get seen until they are in front of a human tester.
+  * prevent creation of empty tasks.
+  * Change edit to save when you are in edit mode.
+  * Problem: User interaction does not provide the correct results.
+  * Solution: Add interactivity so the user can manage daily tasks.
+  * Break things down into smaller steps and take each step at a time.
+  *
+  */
 
 // Event handling, user interaction is what starts the code execution.
 var taskInput = document.getElementById("new-task"); //Add a new task.
@@ -172,9 +176,4 @@ for (var i = 0; i < completedTasksHolder.children.length; i++) {
   bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
 }
 
-//TODO 
-/**
-  * Issues with usability don't get seen until they are in front of a human tester.
-  * prevent creation of empty tasks.
-  * Change edit to save when you are in edit mode.
-  */
+
